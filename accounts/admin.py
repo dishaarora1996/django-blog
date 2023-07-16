e852a299f3892c3f38587635fb4dbbb9c8bd9dc5
Disha Arora <dishaarora1996@gmail.com>
2023-07-16 21:08:47 +0530

    admin panel and dashboard setup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,55 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+
+    def profile_pic(self, obj):
+        return format_html('<img src="{}" width="{}" height="{}"/>'.format(obj.profile_image.url, 30, 30))
+
+    ordering=['id']
+
+    list_display = ('email', 'first_name','profile_pic', 'date_joined', 'last_login','is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('date_joined', 'last_login',)
+
+    filter_horizontal = ()
+    list_filter = ('date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal Info'), {'fields': ('first_name','last_name')}),
+        (
+            ('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (('Important dates'), {'fields': ('last_login','date_joined')}),
+    )
+    readonly_fields = ['last_login', 'date_joined']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'first_name',
+                'last_name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
 
 
 # change Django admin title
@@ -10,4 +59,4 @@ admin.site.site_title = 'Blog Admin'
 admin.site.site_header = 'Blog Admin'
 
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from .models import Blog
 # Create your views here.
+
+
+
+@login_required(login_url='login_user')
+def dashboard(request):
+    blogs = Blog.objects.filter(author = request.user)
+    context ={
+        'blogs': blogs
+    }
+    return render(request, 'dashboard.html', context)
